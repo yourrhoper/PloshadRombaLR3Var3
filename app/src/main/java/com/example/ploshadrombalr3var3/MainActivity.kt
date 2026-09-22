a7e0fba26fdbd6b896e76f54cf3d03a8836a1407
yourrhoper <117834124+yourrhoper@users.noreply.github.com>
2026-09-22 13:51:27 +0700

    Инициализация RadioButton

--- app/src/main/java/com/example/ploshadrombalr3var3/MainActivity.kt
+++ app/src/main/java/com/example/ploshadrombalr3var3/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ploshadrombalr3var3.ui.theme.PloshadRombaLR3Var3Theme
@@ -33,13 +38,39 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RombApp() {
 
-    var variant by remember { mutableStateOf(1) }
-
+    var variant by remember { mutableIntStateOf(1) }
     var a by remember { mutableStateOf("") }
     var h by remember { mutableStateOf("") }
-
     var d1 by remember { mutableStateOf("") }
     var d2 by remember { mutableStateOf("") }
 
     var result by remember { mutableStateOf("") }
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = variant == 1,
+                onClick = {
+                    variant = 1
+                }
+            )
+
+            Text("1. По стороне и высоте")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = variant == 2,
+                onClick = {
+                    variant = 2
+                }
+            )
+
+            Text("2. По диагоналям")
+        }
+    }
 }
+
