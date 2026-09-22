60f43b7450a72d257e5b438279458fe3798def52
yourrhoper <117834124+yourrhoper@users.noreply.github.com>
2026-09-22 13:25:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ploshadrombalr3var3/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ploshadrombalr3var3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ploshadrombalr3var3.ui.theme.PloshadRombaLR3Var3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PloshadRombaLR3Var3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PloshadRombaLR3Var3Theme {
+        Greeting("Android")
+    }
+}
